8fbf41f0f1ec0a2b938f63d5a8d601e3cb11c93c
Chaan0210 <chan060304@gmail.com>
2025-05-07 14:51:45 +0900

    chore: assets 수정

---
+++ assets/characterList.jsx
@@ -0,0 +1,9 @@
+export default [
+    require('./character/char1.png'),
+    require('./character/char2.png'),
+    require('./character/char3.png'),
+    require('./character/char4.png'),
+    require('./character/char5.png'),
+    require('./character/char6.png'),
+    require('./character/char7.png'),
+];
