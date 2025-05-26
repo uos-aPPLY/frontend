ebfdf00ea41fe1e099b55f745958a826fbaffe3f
Chaan0210 <chan060304@gmail.com>
2025-05-26 21:37:50 +0900

    feat: onboarding stack으로 변경

--- app/(onboarding)/_layout.js
+++ app/(onboarding)/_layout.js
@@ -1,7 +1,25 @@
 // app/(onboarding)/_layout.js
-import React from "react";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function OnboardingLayout() {
-  return <Slot />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="terms"
+        options={{ title: "약관 동의", headerShown: false }}
+      />
+      <Stack.Screen
+        name="nickname"
+        options={{ title: "닉네임 설정", headerShown: false }}
+      />
+      <Stack.Screen
+        name="speechstyle"
+        options={{ title: "말투 커스터마이징", headerShown: false }}
+      />
+      <Stack.Screen
+        name="tutorial"
+        options={{ title: "튜토리얼", headerShown: false }}
+      />
+    </Stack>
+  );
 }
