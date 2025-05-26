63ba1ef530b3d306daaf7e933c8135ff035efc38
Chaan0210 <chan060304@gmail.com>
2025-05-26 21:31:34 +0900

    feat: settings speechstyle stack 적용

--- app/settings/_layout.js
+++ app/settings/_layout.js
@@ -1,14 +1,33 @@
 // app/settings/_layout.js
-import React from "react";
 import { Stack } from "expo-router";
 
 export default function SettingsLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "#FCF9F4" },
-      }}
-    />
+    <Stack screenOptions={{ headerShown: false }}>
+      {/* /settings/index.js */}
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/* onboarding 폴더에 있는 컴포넌트를 proxy로 불러옵니다 */}
+      <Stack.Screen
+        name="speechstyle"
+        options={{ title: "말투 커스터마이징" }}
+      />
+      {/* 구독 내역 스크린 */}
+      <Stack.Screen name="subscriptions" options={{ title: "구독 내역" }} />
+      {/* 필요에 따라 다른 /settings 하위 스크린도 등록 */}
+      <Stack.Screen name="help" options={{ title: "문의하기" }} />
+      <Stack.Screen
+        name="termofservice"
+        options={{ title: "서비스 이용약관" }}
+      />
+      <Stack.Screen
+        name="privacypolicy"
+        options={{ title: "개인정보처리방침" }}
+      />
+      <Stack.Screen name="waste" options={{ title: "휴지통" }} />
+      <Stack.Screen
+        name="defaultkeywords"
+        options={{ title: "기본 키워드 설정" }}
+      />
+    </Stack>
   );
 }
