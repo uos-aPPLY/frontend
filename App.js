748fc1a64264113ee96e8f4118779d061b0c30fa
Chaan0210 <chan060304@gmail.com>
2025-06-10 01:21:33 +0900

    feat: notification feature created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import * as Notifications from "expo-notifications";
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowBanner: true,
+    shouldShowList: true,
     shouldPlaySound: true,
     shouldSetBadge: true
   })
